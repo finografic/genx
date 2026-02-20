5f001afb8532fd8ccd999fd5c6ce587420115039
Justin <justin.blair.rankin@gmail.com>
2026-02-20 16:42:45 +0100

    chore: eslint.config and declarations cleanup

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,11 @@
-declare module '@typescript-eslint/eslint-plugin' {
+declare module 'eslint-plugin-markdownlint' {
   import type { Linter } from 'eslint';
   const plugin: Linter.Plugin;
   export default plugin;
+}
+
+declare module 'eslint-plugin-markdownlint/parser.js' {
+  import type { Linter } from 'eslint';
+  const parser: Linter.Parser;
+  export default parser;
 }
