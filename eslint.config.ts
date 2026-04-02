15c4b9c90f99cf2738c68044ec2916d550611e76
Justin <justin.blair.rankin@gmail.com>
2026-04-02 16:46:29 +0200

    fix: eslint.config + oxfmt.config fixes

--- eslint.config.ts
+++ eslint.config.ts
@@ -49,7 +49,7 @@ export default defineConfig([
     plugins: {
       '@typescript-eslint': tseslint.plugin,
       'simple-import-sort': simpleImportSort,
-      stylistic,
+      '@stylistic': stylistic,
     },
     rules: {
       // Disable base rules in favor of TS-aware ones
@@ -57,37 +57,41 @@ export default defineConfig([
       'no-redeclare': 'off',
       'no-console': 'off',
 
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          caughtErrors: 'all',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-        },
-      ],
+      '@typescript-eslint/no-unused-vars': ['error', {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },],
       '@typescript-eslint/no-redeclare': 'warn',
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
-      ],
+      '@typescript-eslint/consistent-type-imports': ['error', {
+        prefer: 'type-imports',
+        fixStyle: 'separate-type-imports',
+      },],
 
-      // 'stylistic/semi': ['error'],
-      // 'stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      // '@stylistic/semi': ['error'],
+      // '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
       // TODO: REMOVE `ConditionalExpression` WHEN oxfmt IS ADDED TO
-      'stylistic/indent': ['warn', 2, { SwitchCase: 1, ignoredNodes: ['ConditionalExpression'] }],
-      // 'stylistic/no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 1 }],
-      // 'stylistic/no-multi-spaces': ['warn', { exceptions: { Property: true } }],
-      // 'stylistic/no-trailing-spaces': 'warn',
-      // 'stylistic/object-curly-spacing': ['error', 'always'],
-      // 'stylistic/comma-spacing': ['error', { before: false, after: true }],
-      // 'stylistic/comma-dangle': ['error', 'only-multiline'],
-      // 'stylistic/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
-      // 'stylistic/arrow-spacing': ['error', { before: true, after: true }],
-      // 'stylistic/type-annotation-spacing': ['error', {
+      '@stylistic/indent': ['warn', 2, { SwitchCase: 1, ignoredNodes: ['ConditionalExpression'] }],
+      // ======================================================================== //
+      '@stylistic/operator-linebreak': ['warn', 'before'],
+      '@stylistic/multiline-ternary': ['warn', 'always-multiline'],
+      '@stylistic/array-bracket-newline': ['warn', { minItems: 4 }],
+      '@stylistic/array-element-newline': ['warn', { minItems: 4 }],
+      // ======================================================================== //
+      // '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 1 }],
+      // '@stylistic/no-multi-spaces': ['warn', { exceptions: { Property: true } }],
+      // '@stylistic/no-trailing-spaces': 'warn',
+      // '@stylistic/object-curly-spacing': ['error', 'always'],
+
+      // '@stylistic/comma-spacing': ['error', { before: false, after: true }],
+      // '@stylistic/comma-dangle': ['error', 'only-multiline'],
+      // '@stylistic/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
+      // '@stylistic/arrow-spacing': ['error', { before: true, after: true }],
+      // '@stylistic/type-annotation-spacing': ['error', {
       //   before: false,
       //   after: true,
       //   // overrides: { 'arrow': 'ignore' },
@@ -139,7 +143,7 @@ export default defineConfig([
     },
     plugins: {
       markdownlint: markdownlintPlugin as Linter.Processor,
-      stylistic,
+      '@stylistic': stylistic,
     },
     // processor: markdownlintPlugin.processors.markdown,
     rules: {
@@ -159,9 +163,9 @@ export default defineConfig([
       'markdownlint/md045': 'off', // images require alt text
 
       // Formatting consistency
-      'stylistic/no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 1 }],
-      'stylistic/no-trailing-spaces': 'error',
-      'stylistic/no-multi-spaces': ['error', { exceptions: { Property: true } }],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 1 }],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/no-multi-spaces': ['error', { exceptions: { Property: true } }],
     },
   },
 
